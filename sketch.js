45b38392eeaddae00677dd2d308cf000ff86eb55
lhj73999 <ehgkfdl@naver.com>
2014-12-11 14:24:59 +0900

    d

---
+++ sketch.js
@@ -0,0 +1,38 @@
+var img;
+var trailX = [0, 0, 0, 0];
+
+var trailY = [0, 0, 0, 0];
+
+
+function setup() 
+{
+  createCanvas(800, 600);
+  noFill();
+  stroke(255, 0, 0);
+
+  img = loadimage("버섯-맞춤.png")
+}
+
+function draw()
+{ 
+  background(0);
+    
+  //mouse ellipse
+  img(mouseX, mouseY, 100, 100);
+  
+  //trail ellipse #0
+  trailX[0] = (mouseX + trailX[0] * 9) / 10;
+  trailY[0] = (mouseY + trailY[0] * 9) / 10;
+  img(trailX[0], trailY[0], 80, 80);
+
+ 
+  for (var i = 1; i < 4; i = i + 1)
+
+{
+	trailX[i] = (trailX[i-1] + trailX[i] * 9) / 10;
+  trailY[i] = (trailY[i-1] + trailY[i] * 9) / 10;
+  img(trailX[i], trailY[i], 80, 80);
+
+}  
+  
+}   
