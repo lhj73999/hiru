bdd1703627c826242ead331db0ad52aea259575e
lhj73999 <ehgkfdl@naver.com>
2014-12-11 14:26:22 +0900

    s

--- sketch.js
+++ sketch.js
@@ -1,7 +1,4 @@
 var img;
-var trailX = [0, 0, 0, 0];
-
-var trailY = [0, 0, 0, 0];
 
 
 function setup() 
@@ -19,20 +16,5 @@ function draw()
     
   //mouse ellipse
   img(mouseX, mouseY, 100, 100);
-  
-  //trail ellipse #0
-  trailX[0] = (mouseX + trailX[0] * 9) / 10;
-  trailY[0] = (mouseY + trailY[0] * 9) / 10;
-  img(trailX[0], trailY[0], 80, 80);
-
- 
-  for (var i = 1; i < 4; i = i + 1)
-
-{
-	trailX[i] = (trailX[i-1] + trailX[i] * 9) / 10;
-  trailY[i] = (trailY[i-1] + trailY[i] * 9) / 10;
-  img(trailX[i], trailY[i], 80, 80);
-
-}  
   
 }   
