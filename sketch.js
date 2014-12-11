16f0241ecdf8a7931c429c6b7cfcc06e0bf32ae7
lhj73999 <ehgkfdl@naver.com>
2014-12-11 14:30:14 +0900

    d

--- sketch.js
+++ sketch.js
@@ -4,17 +4,16 @@ var img;
 function setup() 
 {
   createCanvas(800, 600);
-  noFill();
-  stroke(255, 0, 0);
-
-  img = loadimage("버섯-맞춤.png")
+  img = loadimage("1.png")
 }
 
 function draw()
 { 
   background(0);
-    
+  noFill();
+  nostroke();
+
   //mouse ellipse
-  img(mouseX, mouseY, 100, 100);
+  image(img, 100, 100);
   
 }   
