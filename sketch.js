0a3bbfacdd16be084d4698d36a2c4f1be8ea31dd
lhj73999 <ehgkfdl@naver.com>
2014-12-11 15:30:14 +0900

    sdasdasd

--- sketch.js
+++ sketch.js
@@ -1,24 +1,40 @@
-var playing = false;
-var fingers;
-var button;
-
+var img;
+var bg;
 
 function setup() {
-  // specify multiple formats for different browsers
-  fingers = createVideo(['wpqkf.avi', 
-                         'wpqkf.webm']);
-  button = createButton('play');
-  button.mousePressed(toggleVid); // attach button listener
+  createCanvas(800,800);
+  bg = loadImage("space.jpg");
+  img = loadImage("dog.png");
+  img2 = loadImage("boo.png");
+  img3 = loadImage("mooo.png");
+ 
+
 }
 
-// plays or pauses the video depending on current state
-function toggleVid() {
-  if (playing) {
-    fingers.pause();
-    button.html('play');
-  } else {
-    fingers.loop();
-    button.html('pause');
-  }
-  playing = !playing;
+function draw() {
+   background(bg);
+  
+   fill(255,64,64);
+   noStroke()
+
+   var duration = 4000;
+   var timing = (new Date()%duration)/duration;
+
+   image(img,400 + Math.cos(timing*2*PI)*200-100,       
+        400 + Math.sin(timing*2*PI)*200-100,200,150);  
+ 
+ var duration = 3000;
+   var timing = (new Date()%duration)/duration;
+ 
+        image(img2,400 + Math.cos(timing*2*PI)*200-100,       
+        400 + Math.sin(timing*2*PI)*200-100,100,200);                                 
+
+
+         var duration = 5000;
+   var timing = (new Date()%duration)/duration;
+
+ image(img3,400 + Math.cos(timing*2*PI)*200-100,       
+        400 + Math.sin(timing*2*PI)*200-100,100,100);                                 
+
+
 } 
